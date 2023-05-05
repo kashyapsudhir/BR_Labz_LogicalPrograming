8e6a3ed94bb9b02028c8a3130a35b82508b5b15d
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-05 15:03:13 +0530

    [CouponNumber Programming]

--- LogicalPoblems/Program.cs
+++ LogicalPoblems/Program.cs
@@ -7,8 +7,16 @@ namespace LogicalProblem
     {
         public static void Main(string[] args) 
         {
+           
             Console.WriteLine("Hello Logical Programing Problem");
-            PerfectNumber.CheckPerfectNumber();
+            Console.Write("Enter the value for the coupon number: ");
+            
+            int couponInput = Convert.ToInt32(Console.ReadLine());
+            
+            int number = CouponNumber.FindCouponNumber(couponInput);
+            
+            Console.WriteLine("Coupon Number: " + number);
+
         }
     }
 }
